c39736da202ab7dcec81626f3953e0a8c37a6337
BubbleSort <faridzuhdi28@gmail.com>
2022-07-01 14:50:31 +0700

    bikin page admin laporan masalah

--- app/includes/admin-pelaporan-masalah.php
+++ app/includes/admin-pelaporan-masalah.php
@@ -87,7 +87,7 @@
             <div class="col-md-12">
               <div class="card">
                 <div class="card-header">
-                  <h4>Invoices</h4>
+                  <h4>Laporan Masalah</h4>
                   <div class="card-header-action">
                     <a href="#" class="btn btn-danger">View More <i class="fas fa-chevron-right"></i></a>
                   </div>
@@ -103,7 +103,7 @@
                         <th>Deskripsi Masalah</th>
                       </tr>
                       <tr>
-                        <td><a href="#">INV-87239</a></td>
+                        <td><a href="#">LR-87239</a></td>
                         <td class="font-weight-600">Kusnadi</td>
                         <td class="font-weight-600">Kusnadi</td>
                         <td class="font-weight-600">Kusnadi</td>
@@ -122,7 +122,7 @@
                         </div>
                       </tr>
                       <tr>
-                        <td><a href="#">INV-48574</a></td>
+                        <td><a href="#">LR-48574</a></td>
                         <td class="font-weight-600">Hasan Basri</td>
                         <td class="font-weight-600">Kusnadi</td>
                         <td class="font-weight-600">Kusnadi</td>
@@ -139,7 +139,7 @@
                         </td>
                       </tr>
                       <tr>
-                        <td><a href="#">INV-76824</a></td>
+                        <td><a href="#">LR-76824</a></td>
                         <td class="font-weight-600">Muhamad Nuruzzaki</td>
                         <td class="font-weight-600">Kusnadi</td>
                         <td class="font-weight-600">Kusnadi</td>
@@ -156,7 +156,7 @@
                         </td>
                       </tr>
                       <tr>
-                        <td><a href="#">INV-84990</a></td>
+                        <td><a href="#">LR-84990</a></td>
                         <td class="font-weight-600">Agung Ardiansyah</td>
                         <td class="font-weight-600">Kusnadi</td>
                         <td class="font-weight-600">Kusnadi</td>
@@ -173,7 +173,7 @@
                         </td>
                       </tr>
                       <tr>
-                        <td><a href="#">INV-87320</a></td>
+                        <td><a href="#">LR-87320</a></td>
                         <td class="font-weight-600">Ardian Rahardiansyah</td>
                         <td class="font-weight-600">Kusnadi</td>
                         <td class="font-weight-600">Kusnadi</td>
