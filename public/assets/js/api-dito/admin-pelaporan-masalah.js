2e87690665fc16a550eff77d48b9dbb9299c8966
Rinaldito <rinalditoryanari@gmail.com>
2022-07-15 23:03:28 +0700

    pop up detail masalah

--- public/assets/js/api-dito/admin-pelaporan-masalah.js
+++ public/assets/js/api-dito/admin-pelaporan-masalah.js
@@ -37,40 +37,73 @@ function selesai_btn(){
   })
 }
 
-function detail_btn(n){
-    console.log(n)
-    var id = $(this).closest("tr").find("td:eq(0)").text();
-    swal({
-      title: 'Apakah anda yakin?',
-      text: 'Proses ini tidak bisa diubah kembali seperti awal',
-      icon: 'warning',
-      buttons: true,
-    })
-    .then((willDelete) => {
-      if (willDelete) {
-        let tokenSession = sessionStorage.getItem("token");
-        let token = "Bearer" + " " + tokenSession;
-        const url = "https://pepeseal.klubaderai.com/api/adminselesailaporan/"+id
+function detail_btn(n){  
+    let tokenSession = sessionStorage.getItem("token");
+    let token = "Bearer" + " " + tokenSession;
+    var a;
+    switch (n) {
+      case 1:
+        a = "#tablePelaporanMasalah"
+        break;
+      case 2:
+        a = "#tablePelaporanMasalahSelesai"
+    }
+    $(a).on('click', '#btnDetail', function() {
+      var id = $(this).closest("tr").find("td:eq(0)").text();
+      const url = "https://pepeseal.klubaderai.com/api/getlaporanmasalahuser/" + id
+      $(document).ready(function () {
         $.ajax({
-          method: "PUT",
-          url: url,
-          headers: {
-              Authorization: token,
-          },
-          success: function (response){
-              swal('Pengangkutan akan dilaksanakan', {
-                icon: 'success',
-              });
-          },
-          error: function (response) {
-              var hasil = response.responseJSON.message;
-              alert(hasil);
-          },
-      });
-      } else {
-      swal('Proses dibatalkan');
-      }
+            method: "GET",
+            url: url,
+            headers: {
+                Authorization: token,
+            },
+            success: function (response) {
+              var dataLaporan = response.laporan
+               swal({
+                title: "Detail Masalah",
+                text: dataLaporan.laporan
+               })
+            },
+            error: function (response) {
+                var hasil = response.responseJSON.message;
+                alert(hasil);
+            },
+        });
     });
+    })
+
+    // swal({
+    //   title: 'Apakah anda yakin?',
+    //   text: 'Proses ini tidak bisa diubah kembali seperti awal',
+    //   icon: 'warning',
+    //   buttons: true,
+    // })
+    // .then((willDelete) => {
+    //   if (willDelete) {
+    //     let tokenSession = sessionStorage.getItem("token");
+    //     let token = "Bearer" + " " + tokenSession;
+    //     const url = "https://pepeseal.klubaderai.com/api/adminselesailaporan/"+id
+    //     $.ajax({
+    //       method: "PUT",
+    //       url: url,
+    //       headers: {
+    //           Authorization: token,
+    //       },
+    //       success: function (response){
+    //           swal('Pengangkutan akan dilaksanakan', {
+    //             icon: 'success',
+    //           });
+    //       },
+    //       error: function (response) {
+    //           var hasil = response.responseJSON.message;
+    //           alert(hasil);
+    //       },
+    //   });
+    //   } else {
+    //   swal('Proses dibatalkan');
+    //   }
+    // });
 }
 
 function tablePelaporanMasalah() {
@@ -123,8 +156,8 @@ function tablePelaporanMasalah() {
                     {
                       targets:[7],
                       render: function () {
-                          const btnSelesai = '<button class="btn btn-success mx-1" id="btnSelesai" onclick="selesai_btn()">Selesai</button>'
-                          const btnDetail = '<button class="btn btn-info mx-1" id="btnDetail" onclick="detail_btn(1)">Detail</button>' 
+                          const btnSelesai = '<button class="btn btn-success mx-1 mb-2" id="btnSelesai" onclick="selesai_btn()">Selesai</button>'
+                          const btnDetail = '<button class="btn btn-info mx-1 mb-2" id="btnDetail" onclick="detail_btn(1)">Detail</button>' 
                           return btnSelesai + btnDetail   
                       }
                     }],
